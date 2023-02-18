162b60760b57f6e70f267ae6d60f70ceffb400f7
a.simakov <a.simakov@bag24.ru>
2023-02-18 07:24:41 +0300

    fix: отладка

--- moika_server/settings.py
+++ moika_server/settings.py
@@ -86,24 +86,24 @@ WSGI_APPLICATION = 'moika_server.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 # Тестовая БД
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# Продакшн БД
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DBNAME'),
-        'USER': env('DBUSER'),
-        'PASSWORD': env('DBPASSWORD'),
-        'HOST': env('DBHOST'),
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# Продакшн БД
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': env('DBNAME'),
+#         'USER': env('DBUSER'),
+#         'PASSWORD': env('DBPASSWORD'),
+#         'HOST': env('DBHOST'),
+#         'PORT': '',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
